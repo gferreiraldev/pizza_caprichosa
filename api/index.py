1086f52f3f9d05eb7e96805d6790535ffe9c5baf
gferreiraldev <g.ferreira.ldev@gmail.com>
2026-08-13 15:24:21 -0300

    Delete api directory

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Permite que o Python encontre o seu app.py na raiz do projeto
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app import app
